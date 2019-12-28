591c5f5e9c2d9fabb1445617e09ffddc1ed6eecb
stellar-class-3593 <csamanta95@gmail.com>
2019-12-28 21:51:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,7 +5,7 @@
 def introduction(name)
   "Hi, my name is #{name}."
 end
-introduction("Samanta")
+
 def introduction_with_language(name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
